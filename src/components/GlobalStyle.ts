09756b275e264495998b14b454ad022973d12392
Lucas <bellierlucas.pro@gmail.com>
2023-10-20 15:18:40 +0200

    💄 update theme and package manager

--- src/components/GlobalStyle.ts
+++ src/components/GlobalStyle.ts
@@ -202,7 +202,6 @@ export default createGlobalStyle`
 
         :hover {
             filter: brightness(0.8);
-       
         }
     }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import { HomeScreen } from "@screens/Homepage";
-
 export default function Home() {
-  // ! Delete the HomeScreen component to remove the landing page
-  return <HomeScreen />;
+  return <>Test</>;
 }
