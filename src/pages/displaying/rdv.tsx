1b641bec90135a0169213fc71d4527a172caca6e
ЯΛYИΛR <rayan.idri@edu.devinci.fr>
2024-01-15 19:02:05 +0100

    Visual updated - New data - Better UI

--- src/pages/displaying/rdv.tsx
+++ src/pages/displaying/rdv.tsx
@@ -8,6 +8,12 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const RdvDetail = styled.li`
+  border: 1px solid #000;
+  padding: 1rem;
+  margin: 1rem;
+`;
+
 export default function DisplayRdv() {
   const [data, setData] = useState<
     {
@@ -15,13 +21,34 @@ export default function DisplayRdv() {
       attributes: {
         Date: string;
         Status: boolean;
+        createdAt: string;
+        updatedAt: string;
+        Employee: {
+          data: {
+            attributes: {
+              Name: string;
+              Lastname: string;
+              Function: string;
+              Email: string;
+            };
+          };
+        };
+        Humanresource: {
+          data: {
+            attributes: {
+              Name: string;
+              Lastname: string;
+              Email: string;
+            };
+          };
+        };
       };
     }[]
   >([]);
 
   useEffect(() => {
     redaxios
-      .get("http://localhost:1337/api/rdvs")
+      .get("http://localhost:1337/api/rdvs?populate=*")
       .then((res) => {
         setData(res.data.data);
       })
@@ -30,13 +57,37 @@ export default function DisplayRdv() {
       });
   }, []);
 
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  const getStatusText = (status: boolean) => {
+    return status ? "RDV terminé ✅" : "RDV non encore fait ⚠️";
+  };
+
   return (
     <Container>
       <ul>
         {data.map((item) => (
-          <li key={item.id}>
-            {item.attributes.Date} - {item.attributes.Status.toString()}
-          </li>
+          <RdvDetail key={item.id}>
+            {formatDate(item.attributes.Date)}
+            <br />
+            {getStatusText(item.attributes.Status)}
+            <br />
+            <p>
+              <u>Interviewer:</u>{" "}
+              {item.attributes.Humanresource.data.attributes.Name}{" "}
+              {item.attributes.Humanresource.data.attributes.Lastname}
+            </p>
+            <p>
+              <u>Employee:</u> {item.attributes.Employee.data.attributes.Name}{" "}
+              {item.attributes.Employee.data.attributes.Lastname}
+            </p>
+          </RdvDetail>
         ))}
       </ul>
     </Container>
