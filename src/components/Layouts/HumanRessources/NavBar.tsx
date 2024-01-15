8293c2c996f1c15f98203f9c4fd668678926db28
Lucas <bellierlucas.pro@gmail.com>
2024-01-15 23:41:01 +0100

    💄 update style of top and nav bar

--- src/components/Layouts/HumanRessources/NavBar.tsx
+++ src/components/Layouts/HumanRessources/NavBar.tsx
@@ -15,5 +15,6 @@ const NavBarContainer = styled.div`
   position: fixed;
   top: 0;
   bottom: 0;
-  z-index: 99998;
+  z-index: 100;
+  background: red;
 `;
--- src/components/Layouts/HumanRessources/TopBar.tsx
+++ src/components/Layouts/HumanRessources/TopBar.tsx
@@ -10,4 +10,12 @@ export const TopBar: React.FC = () => {
 
 const TopBarContainer = styled.div`
   height: 88px;
+  width: 100%;
+  user-select: none;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  z-index: 99;
+  background: blue;
+  padding-left: 250px;
 `;
