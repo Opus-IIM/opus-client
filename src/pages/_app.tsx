fd57babe5ff0a9802d54f6958dc3cfc10a2cb02c
Lucas <bellierlucas.pro@gmail.com>
2024-01-18 00:21:49 +0100

    🔥 remove old layout provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "@styles/main.css";
 
 import React from "react";
 import { GlobalThemeProvider } from "@contexts/GlobalTheme";
-import { LayoutProvider } from "@contexts/LayoutContext";
 import { AppLayout } from "@layouts/AppLayout";
 import GlobalStyle from "@styles/GlobalStyle";
 import { CustomAppProps } from "@typesDef/APP";
@@ -14,17 +13,15 @@ export default function App({ Component, pageProps }: CustomAppProps) {
     Component.getLayout || ((page) => <AppLayout>{page}</AppLayout>);
   return (
     <GlobalThemeProvider>
-      <LayoutProvider>
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover, height=device-height"
-          />
-          <title>Opus</title>
-        </Head>
-        <GlobalStyle />
-        {getLayout(<Component {...pageProps} />)}
-      </LayoutProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover, height=device-height"
+        />
+        <title>Opus</title>
+      </Head>
+      <GlobalStyle />
+      {getLayout(<Component {...pageProps} />)}
     </GlobalThemeProvider>
   );
 }
