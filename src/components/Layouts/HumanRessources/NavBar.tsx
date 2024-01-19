46e5f9f9b2c2bf4f06088182b2956ec3ead9f2b8
pierre_cours <pierre_cours@MacBook-Pro-80.local>
2024-01-19 15:29:46 +0100

    :sparkles:

--- src/components/Layouts/HumanRessources/NavBar.tsx
+++ src/components/Layouts/HumanRessources/NavBar.tsx
@@ -1,20 +1,39 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
+import Image from "next/image";
+
+import logo from "../../../../public/img/logo.svg";
 
 export const NavBar: React.FC = () => {
   return (
     <NavBarContainer>
-      <h1>NavBar</h1>
+      <LogoContainer>
+        <Image
+          src={logo}
+          alt="OPUS Logo"
+          layout="intrinsic" // Cette propriété permet de conserver le ratio d'aspect
+          width={100} // Taille originale en largeur de l'image
+          height={100} // Taille originale en hauteur de l'image
+        />
+      </LogoContainer>
     </NavBarContainer>
   );
 };
 
 const NavBarContainer = styled.div`
   height: 100vh;
-  width: 250px;
+  width: calc(250px - 32px);
   user-select: none;
   position: fixed;
   top: 0;
   bottom: 0;
   z-index: 100;
-  background: red;
+  background-color: ${({ theme }) => theme.colors.white};
+  padding-left: 32px;
+`;
+
+const LogoContainer = styled.div`
+  padding-top: calc(44px - 15px);
+  width: 100%;
+  height: calc(44px + 15px);
+  border-bottom: solid 1px ${({ theme }) => theme.colors.light};
 `;
