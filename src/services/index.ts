db3dd89a43bf0314dca31d3d13474d51494b8645
Nico <nicobdx@icloud.com>
2024-01-19 14:04:54 +0100

    merge login & register in auth

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,15 +1,13 @@
+import authApi, { authAPI } from "./api/auth";
 import employeesApi, { employeesAPI } from "./api/employees";
-import loginApi, { loginAPI } from "./api/login";
-import registerApi, { registerAPI } from "./api/register";
 type SERVICESType = {
-  API: employeesAPI & registerAPI & loginAPI;
+  API: employeesAPI & authAPI;
 };
 
 const SERVICES: SERVICESType = {
   API: {
     ...employeesApi,
-    ...registerApi,
-    ...loginApi,
+    ...authApi,
   },
 };
 
