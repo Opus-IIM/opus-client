0c8effa973d5526e083f19fd28b9c7b36ca8b775
Lucas <bellierlucas.pro@gmail.com>
2024-01-19 14:57:41 +0100

    💄 add style employee profile

--- src/components/Pages/HumanRessources/EmployeeProfile/index.tsx
+++ src/components/Pages/HumanRessources/EmployeeProfile/index.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+import { CardContainer } from "@components/common/Container/CardContainer";
+import { MenuItem } from "@components/common/Menu/MenuItem";
 import SERVICES from "@services/index";
+import { BoldText } from "@styles/components/Text";
 import { APIResponse } from "@typesDef/API";
 import { Employee } from "@typesDef/Employee";
 import { styled } from "styled-components";
 
+import { EmployeeContent } from "./EmployeeContent";
 import { EmployeeProfileHeader } from "./Header";
 
 type EmployeeProfileProps = {
@@ -34,6 +38,22 @@ export const EmployeeProfileScreen: React.FC<EmployeeProfileProps> = ({
   return (
     <EmployeeProfileContainer>
       <EmployeeProfileHeader />
+      <FlexContainer>
+        <ColumnContainer>
+          <CardContainer>
+            <MenuItem>Biography</MenuItem>
+            <MenuItem>Compétences</MenuItem>
+            <MenuItem>Expérience</MenuItem>
+            <MenuItem>Formation</MenuItem>
+            <MenuItem>Contact</MenuItem>
+            <MenuItem>Recommandation</MenuItem>
+          </CardContainer>
+          <CardContainer>
+            <BoldText>A propos</BoldText>
+          </CardContainer>
+        </ColumnContainer>
+        <EmployeeContent />
+      </FlexContainer>
     </EmployeeProfileContainer>
   );
 };
@@ -41,4 +61,22 @@ export const EmployeeProfileScreen: React.FC<EmployeeProfileProps> = ({
 const EmployeeProfileContainer = styled.div`
   background: ${({ theme }) => theme.colors.light};
   padding: 24px;
+  min-height: calc(100vh - 136px);
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 32px;
+`;
+
+const ColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 `;
