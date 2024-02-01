fe30980e803bc72e3f45b1cd60506a703ff14207
IDRAYNAR <rayan.idri@edu.devinci.fr>
2024-02-01 21:49:20 +0100

    Visuals Reworked - New Style Added

--- src/pages/displaying-DEV/dashboard-demo.tsx
+++ src/pages/displaying-DEV/dashboard-demo.tsx
@@ -1,11 +1,4 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import { DefaultizedPieValueType } from "@mui/x-charts";
-import { pieArcLabelClasses, PieChart } from "@mui/x-charts/PieChart";
-import { DataGrid } from "@mui/x-data-grid";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import styled from "styled-components";
 
 const MainPage = styled.div`
@@ -16,8 +9,13 @@ const MainPage = styled.div`
     font-weight: bold;
   }
 
-  .HeaderAndContent {
+  .headerAndContent {
     width: 85vw;
+  }
+
+  hr {
+    border: 2px solid #f8f9fa;
+    width: 100%;
   }
 `;
 
@@ -31,16 +29,30 @@ const SideBar = styled.div`
   padding: 3rem 1.5rem;
   border-right: 3px solid #f8f9fa;
 
+  .appName {
+    font-family: adieu;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
   hr {
-    border: 2px solid #f8f9fa;
-    width: 100%;
     margin: 2rem 0rem 1.5rem 0rem;
   }
 
-  .SidebarMenu {
+  .lastHr {
+    margin-bottom: 0;
+  }
+
+  .sidebarMenu {
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
+
+    .menuTitle {
+      font-size: 0.9rem;
+      font-weight: bold;
+      color: #64748b;
+    }
 
     a {
       background-color: transparent;
@@ -48,12 +60,24 @@ const SideBar = styled.div`
       margin-top: 0.5rem;
       align-items: center;
       display: flex;
-      gap: 0.5rem;
-      width: 100%;
+      gap: 1rem;
+      padding: 0.5rem 1rem;
+      width: 13rem;
       font-size: 1.25rem;
-      color: black;
+      color: #64748b;
+
+      i {
+        font-size: 1.5rem;
+      }
+
+      p {
+        padding: 0 0 0.25rem 0;
+      }
 
       &:hover {
+        border-radius: 1rem;
+        background-color: #f8fafc;
+        color: #1e73be;
         cursor: pointer;
       }
     }
@@ -68,17 +92,17 @@ const Header = styled.div`
   height: 8vh;
   padding: 0rem 1.5rem;
 
-  .RightElements {
+  .rightElements {
     display: flex;
     align-items: center;
-    gap: 1.5rem;
+    gap: 2rem;
 
     input {
-      width: 20rem;
+      width: 15rem;
       height: 2rem;
-      border-radius: 0.5rem;
+      border-radius: 1rem;
       border: 2px solid #f8f9fa;
-      padding-left: 0.5rem;
+      padding: 0.5rem 1.5rem;
     }
 
     button {
@@ -91,7 +115,7 @@ const Header = styled.div`
         }
     }
 
-    .ProfileBtn {
+    .profileBtn {
       display: flex;
       align-items: center;
       gap: 0.5rem;
@@ -102,18 +126,18 @@ const Header = styled.div`
         border-radius: 50%;
       }
 
-      .IdAndBtn {
+      .idAndBtn {
         display: flex;
         align-items: center;
         gap: 1rem;
       }
 
-      .UserId {
+      .userId {
         display: flex;
         align-items: flex-start;    
         flex-direction: column;
 
-        .Name {
+        .name {
             font-weight: bold;
         }
     }
@@ -131,206 +155,321 @@ const DashboardContent = styled.div`
   justify-items: center;
   height: 85.05vh;
 
-  .EmployeeStatus {
+  .employeeStatus {
     grid-area: 1 / 1 / 2 / 2;
   }
 
-  .ReconversionSucceed {
+  .reconversionSucceed {
     grid-area: 2 / 1 / 3 / 2;
   }
 
-  .ReconversionInProgress {
+  .reconversionInProgress {
     grid-area: 3 / 1 / 4 / 2;
   }
 
-  .EmployeeRdvs {
+  .employeeRdvs {
     grid-area: 1 / 2 / 2 / 3;
   }
 
-  .ReconversionFailed {
+  .reconversionFailed {
     grid-area: 2 / 2 / 3 / 3;
   }
 
-  .ReconversionElegibility {
+  .reconversionElegibility {
     grid-area: 3 / 2 / 4 / 3;
   }
 
-  .Calendar {
-    padding-top: 1rem;
+  .calendar {
     grid-area: 1 / 3 / 2 / 4;
   }
 
-  .RdvsList {
+  .rdvsList {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     grid-area: 2 / 3 / 4 / 4;
+
+    hr {
+      margin: 0 0 0.5rem 0;
+    }
+
+    .rdvDisplayList {
+      display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+
+      .title {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+      }
+
+      .rdvs {
+        gap: 0.25rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+    }
+
+    .seeAll {
+      text-align: center;
+      text-decoration: none;
+      color: #000000;
+      font-weight: bold;
+      font-size: 1rem;
+      margin: 0.5rem 0 0 0;
+    }
   }
 `;
 
 const DataBlock = styled.div`
   background-color: #ffffff;
   border-radius: 0.5rem;
-  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1.5rem;
   width: 85%;
+
+  .category {
+    font-weight: bold;
+    color: #64748b;
+  }
+
+  .rfidAndFull {
+    display: flex;
+    justify-content: space-between;
+    .rfid {
+      color: #f6a723;
+      background-color: #fffbeb;
+      border-radius: 1rem;
+      padding: 1rem;
+      i {
+        font-size: 1.5rem;
+      }
+    }
+    .full {
+      padding: 0.5rem;
+
+      :hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .data {
+    display: flex;
+    gap: 1.5rem;
+    align-items: center;
+
+    .percentage {
+      color: #24d164;
+      background-color: #f0fdf4;
+      border-radius: 1rem;
+      padding: 0.25rem 0.5rem;
+      font-size: 0.9rem;
+    }
+  }
 `;
 
-const data = [
-  { label: "Urgent", value: 8, color: "#FF0000" },
-  { label: "A surveiller", value: 12, color: "#FFBB28" },
-  { label: "Neutre", value: 23, color: "#0088FE" },
-];
+const Rdv = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.25rem 0.5rem;
+  gap: 1rem;
 
-const sizing = {
-  margin: { right: 0 },
-  width: 425,
-  height: 325,
-  legend: { hidden: false },
-};
-const TOTAL = data.map((item) => item.value).reduce((a, b) => a + b, 0);
+  img {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+  }
 
-const getArcLabel = (params: DefaultizedPieValueType) => {
-  const percent = params.value / TOTAL;
-  return `${(percent * 100).toFixed(0)}%`;
-};
+  .agentAndDate {
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+
+    .agent {
+      font-weight: bold;
+    }
+
+    .date {
+      color: #64748b;
+      font-size: 0.9rem;
+    }
+  }
+`;
 
 export default function Dashboard() {
   return (
     <MainPage>
       <SideBar>
         <div>
-          <h1>Opus</h1>
+          <div>
+            <h1 className="appName">Opus</h1>
+          </div>
           <hr />
-          <div className="SidebarMenu">
-            <p>Menu</p>
+          <div className="sidebarMenu">
+            <p className="menuTitle">MENU</p>
             <div>
               <a href="#">
-                <p>🌐</p>Dashboard
+                <i className="ri-apps-2-fill"></i>
+                <p>Dashboard</p>
               </a>
             </div>
             <div>
               <a href="#">
-                <p>📝</p>RDV
+                <i className="ri-checkbox-line"></i>
+                <p>Mes rendez-vous</p>
               </a>
             </div>
             <div>
               <a href="#">
-                <p>💼</p>
-                Employees
+                <i className="ri-chat-voice-line"></i>
+                <p>Les Employés</p>
               </a>
             </div>
           </div>
           <hr />
         </div>
-        <div className="SidebarMenu">
-          <hr />
+        <div className="sidebarMenu">
+          <hr className="lastHr" />
           <a>
-            <p>⚙️</p>
-            Settings
+            <i className="ri-settings-4-line"></i>
+            <p>Settings</p>
           </a>
         </div>
       </SideBar>
-      <section className="HeaderAndContent">
+      <section className="headerAndContent">
         <Header>
           <div>
             <h1>Dashboard</h1>
           </div>
-          <div className="RightElements">
+          <div className="rightElements">
             <div>
               <input type="text" placeholder="Search..." />
             </div>
-            <button>🔔</button>
-            <button className="ProfileBtn">
+            <i className="ri-notification-2-line"></i>
+            <button className="profileBtn">
               <img src="https://thispersondoesnotexist.com/" />
-              <div className="IdAndBtn">
-                <div className="UserId">
-                  <p className="Name">Angela L.</p>
+              <div className="idAndBtn">
+                <div className="userId">
+                  <p className="name">Angela L.</p>
                   <p>Project Manager</p>
                 </div>
-                <p>🔽</p>
+                <i className="ri-arrow-down-s-line"></i>
               </div>
             </button>
           </div>
         </Header>
         <DashboardContent>
-          <DataBlock className="EmployeeStatus">
-            <PieChart
-              series={[
-                {
-                  outerRadius: 80,
-                  data,
-                  arcLabel: getArcLabel,
-                },
-              ]}
-              sx={{
-                [`& .${pieArcLabelClasses.root}`]: {
-                  fill: "white",
-                  fontSize: 14,
-                },
-              }}
-              {...sizing}
-            />
+          <DataBlock className="employeeStatus">EmployeeStatus</DataBlock>
+          <DataBlock className="employeeRdvs">EmployeeRdvs</DataBlock>
+          <DataBlock className="calendar">Calendar</DataBlock>
+          <DataBlock className="reconversionSucceed">
+            <div className="rfidAndFull">
+              <div className="rfid">
+                <i className="ri-rfid-line"></i>
+              </div>
+              <div className="full">
+                <i className="ri-fullscreen-line"></i>
+              </div>
+            </div>
+            <p className="category">Reconversion réussie</p>
+            <div className="data">
+              <h1>234</h1>
+              <p className="percentage">
+                <i className="ri-arrow-up-line"></i>59%
+              </p>
+            </div>
           </DataBlock>
-          <DataBlock className="EmployeeRdvs">
-            <div></div>
+          <DataBlock className="reconversionFailed">
+            <div className="rfidAndFull">
+              <div className="rfid">
+                <i className="ri-rfid-line"></i>
+              </div>
+              <div className="full">
+                <i className="ri-fullscreen-line"></i>
+              </div>
+            </div>
+            <p className="category">Reconversion en échec</p>
+            <div className="data">
+              <h1>21</h1>
+              <p className="percentage">
+                <i className="ri-arrow-up-line"></i>59%
+              </p>
+            </div>
           </DataBlock>
-          <DataBlock className="Calendar">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DateCalendar />
-            </LocalizationProvider>
+          <DataBlock className="reconversionInProgress">
+            <div className="rfidAndFull">
+              <div className="rfid">
+                <i className="ri-rfid-line"></i>
+              </div>
+              <div className="full">
+                <i className="ri-fullscreen-line"></i>
+              </div>
+            </div>
+            <p className="category">En cours de reconversion</p>
+            <div className="data">
+              <h1>41</h1>
+              <p className="percentage">
+                <i className="ri-arrow-up-line"></i>59%
+              </p>
+            </div>
           </DataBlock>
-          <DataBlock className="ReconversionSucceed">
-            ReconversionSucceed
+          <DataBlock className="reconversionElegibility">
+            <div className="rfidAndFull">
+              <div className="rfid">
+                <i className="ri-rfid-line"></i>
+              </div>
+              <div className="full">
+                <i className="ri-fullscreen-line"></i>
+              </div>
+            </div>
+            <p className="category">Éligible à la reconversion</p>
+            <div className="data">
+              <h1>542</h1>
+              <p className="percentage">
+                <i className="ri-arrow-up-line"></i>59%
+              </p>
+            </div>
           </DataBlock>
-          <DataBlock className="ReconversionFailed">
-            ReconversionFailed
-          </DataBlock>
-          <DataBlock className="ReconversionInProgress">
-            ReconversionInProgress
-          </DataBlock>
-          <DataBlock className="ReconversionElegibility">
-            ReconversionElegibility
-          </DataBlock>
-          <DataBlock className="RdvsList">
-            <Box sx={{ height: 365, width: "100%" }}>
-              <DataGrid
-                columns={[{ field: "username" }, { field: "job" }]}
-                rows={[
-                  {
-                    id: 1,
-                    username: "jean-michel@demo.com",
-                    job: "comptable",
-                  },
-                  {
-                    id: 2,
-                    username: "marie-claire@demo.com",
-                    job: "infirmière",
-                  },
-                  {
-                    id: 3,
-                    username: "pierre@demo.com",
-                    job: "policier",
-                  },
-                  {
-                    id: 4,
-                    username: "sophie@demo.com",
-                    job: "enseignante",
-                  },
-                  {
-                    id: 5,
-                    username: "luc@demo.com",
-                    job: "pompier",
-                  },
-                  {
-                    id: 6,
-                    username: "antoine@demo.com",
-                    job: "militaire",
-                  },
-                  {
-                    id: 7,
-                    username: "claire@demo.com",
-                    job: "médecin",
-                  },
-                ]}
-              />
-            </Box>
+          <DataBlock className="rdvsList">
+            <div className="rdvDisplayList">
+              <div className="title">
+                <h1>Vos prochains RDV</h1>
+                <hr />
+              </div>
+              <div className="rdvs">
+                <Rdv>
+                  <img src="https://thispersondoesnotexist.com/" />
+                  <div className="agentAndDate">
+                    <p className="agent">Andreana Viola</p>
+                    <p className="date">12/05/23 - 08:30</p>
+                  </div>
+                </Rdv>
+                <hr />
+                <Rdv>
+                  <img src="https://thispersondoesnotexist.com/" />
+                  <div className="agentAndDate">
+                    <p className="agent">Andreana Viola</p>
+                    <p className="date">12/05/23 - 08:30</p>
+                  </div>
+                </Rdv>
+                <hr />
+                <Rdv>
+                  <img src="https://thispersondoesnotexist.com/" />
+                  <div className="agentAndDate">
+                    <p className="agent">Andreana Viola</p>
+                    <p className="date">12/05/23 - 08:30</p>
+                  </div>
+                </Rdv>
+              </div>
+            </div>
+            <a href="#" className="seeAll">
+              See All
+            </a>
           </DataBlock>
         </DashboardContent>
       </section>
